46c318c599cd6482e69910d4ec5f811f10f9737e
Harshit Thakur <96690206+harshitthakur7168@users.noreply.github.com>
2022-08-18 23:05:55 +0530

    Create sr_ff.v

---
+++ flipflops/sr_ff.v
@@ -0,0 +1,14 @@
+module srff(q, qbar, s, r, clk);
+
+input s,r,clk; 
+output q, qbar;
+
+wire nand1_out; // output of nand1 
+wire nand2_out; // output of nand2 
+
+nand (nand1_out,clk,s); 
+nand (nand2_out,clk,r); 
+nand (q,nand1_out,qbar);
+nand (qbar,nand2_out,q);
+
+endmodule
